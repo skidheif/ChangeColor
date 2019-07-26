93bbd29ee1580f5231e1d2a2d77f2f7930fc002b
John Heifetz <skidheif@gmail.com>
2019-07-26 12:39:41 +0300

    add code for rgb

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -1,20 +1,109 @@
-//
 //  ViewController.swift
 //  ChangeColor
 //
 //  Created by John Heifetz on 26/07/2019.
 //  Copyright © 2019 heif_team. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewColorField: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var redTextField: UITextField!
+    
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var greenTextField: UITextField!
+    
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Setup main view
+        viewColorField.layer.cornerRadius = 15
+        updateColor()
+        
+        // Setup red label
+        redLabel.font = redLabel.font.withSize(13)
+        redLabel.textColor = .white
+        redLabel.text = "Red:   " + String(redSlider.value)
+        
+        // Setup red slider
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+        redSlider.maximumTrackTintColor = .lightGray
+        
+        //Setup red text field
+        redTextField.keyboardType = .decimalPad
+        redTextField.text = String(redSlider.value)
+        
+        // Setup green label
+        greenLabel.font = greenLabel.font.withSize(13)
+        greenLabel.textColor = .white
+        greenLabel.text = "Green:   " + String(greenSlider.value)
+        
+        // Setup green slider
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.maximumTrackTintColor = .lightGray
+        
+        //Setup green text field
+        greenTextField.keyboardType = .decimalPad
+        greenTextField.text = String(redSlider.value)
+        
+        // Setup blue label
+        blueLabel.font = blueLabel.font.withSize(13)
+        blueLabel.textColor = .white
+        blueLabel.text = "Blue:   " + String(blueSlider.value)
+        
+        // Setup blue slider
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.maximumTrackTintColor = .lightGray
+        
+        //Setup blue text field
+        blueTextField.keyboardType = .decimalPad
+        blueTextField.text = String(redSlider.value)
     }
-
-
+    
+    
+    @IBAction func redSliderAction() {
+        redTextField.text = String(format: "%.2f", redSlider.value)
+        redLabel.text = "Red:   " + String(format: "%.2f", redSlider.value)
+        updateColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        greenLabel.text = "Green:  " + String(format: "%.2f", greenSlider.value)
+        updateColor()
+    }
+    @IBAction func blueSliderAction() {
+        blueTextField.text = String(format: "%.2f", blueSlider.value)
+        blueLabel.text = "Blue:  " + String(format: "%.2f", blueSlider.value)
+        updateColor()
+    }
+    
+    func updateColor(){
+        let redAmount = CGFloat(redSlider.value)
+        let greenAmount = CGFloat(greenSlider.value)
+        let blueAmount = CGFloat(blueSlider.value)
+        
+        let color = UIColor(red: redAmount,
+                            green: greenAmount,
+                            blue: blueAmount,
+                            alpha:  1.0)
+        viewColorField.backgroundColor = color
+    }
 }
-
