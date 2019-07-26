aa71e6241491c5576f6ec00964e40ddc34d653c5
John Heifetz <skidheif@gmail.com>
2019-07-26 16:50:36 +0300

    fix small bug

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
         
         //Setup green text field
         greenTextField.keyboardType = .decimalPad
-        greenTextField.text = String(redSlider.value)
+        greenTextField.text = String(greenSlider.value)
         
         // Setup blue label
         blueLabel.font = blueLabel.font.withSize(13)
@@ -74,7 +74,7 @@ class ViewController: UIViewController {
         
         //Setup blue text field
         blueTextField.keyboardType = .decimalPad
-        blueTextField.text = String(redSlider.value)
+        blueTextField.text = String(blueSlider.value)
         
         //Setup modifyed keyboard
         self.addDoneButtonOnKeyboard()
@@ -111,11 +111,22 @@ class ViewController: UIViewController {
     }
     
     func addDoneButtonOnKeyboard() {
-        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(
+            x: 0,
+            y: 0,
+            width: UIScreen.main.bounds.width,
+            height: 50))
         doneToolbar.barStyle = .default
         
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let flexSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(
+            title: "Done",
+            style: .done,
+            target: self,
+            action: #selector(self.doneButtonAction))
         
         let items = [flexSpace, done]
         doneToolbar.items = items
